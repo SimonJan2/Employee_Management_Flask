13c56540b4840078a768f15acf9767a457d17160
SimonJan2 <119017867+SimonJan2@users.noreply.github.com>
2024-09-05 00:34:04 +0300

    test

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = 'main.login'
